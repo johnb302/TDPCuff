c4b7dcdb1167f85934c483dc9d37ab546cf8a7e2
Jason <jyl26@pitt.edu>
2023-07-24 09:14:07 -0400

    Revisions for stop condition

--- arduino.py
+++ arduino.py
@@ -16,42 +16,43 @@ class Arduino:
         print(f'{self.name} preparing to run...')
         self.serial.reset_input_buffer()
         time.sleep(2) #Allow time for Arduinos to prepare
-        temp_start = time.time()
 
         while self.run:
+            try:
+                # Get the current time
+                now4 = datetime.now()
+                time_string4 = now4.strftime("%H:%M:%S:%f")
+                self.serial.write(b'A4\n')  # Request reading from A4
+                value_A4 = float(self.serial.read_until(expected=b'\n').decode().strip())
+                self.dq.put([self.name+'4', value_A4])
+                print(f"-{self.name}4-{time_string4}T: {value_A4}")
+                barrier.wait(timeout=5)
 
-            # Get the current time
-            now4 = datetime.now()
-            time_string4 = now4.strftime("%H:%M:%S:%f")
-            self.serial.write(b'A4\n')  # Request reading from A4
-            value_A4 = float(self.serial.read_until(expected=b'\n').decode().strip())
-            self.dq.put([self.name+'4', value_A4])
-            print(f"-{self.name}4-{time_string4}T: {value_A4}")
-            barrier.wait()
+                now5 = datetime.now()
+                time_string5 = now5.strftime("%H:%M:%S:%f")
+                self.serial.write(b'A5\n') #A5 Reading
+                value_A5 = float(self.serial.read_until(expected=b'\n').decode().strip())
+                self.dq.put([self.name+'5',value_A5])
+                print(f"-{self.name}5-{time_string5}T: {value_A5}")
+                barrier.wait(timeout=5)
 
-            now5 = datetime.now()
-            time_string5 = now5.strftime("%H:%M:%S:%f")
-            self.serial.write(b'A5\n') #A5 Reading
-            value_A5 = float(self.serial.read_until(expected=b'\n').decode().strip())
-            self.dq.put([self.name+'5',value_A5])
-            print(f"-{self.name}5-{time_string5}T: {value_A5}")
-            barrier.wait()
+                now6 = datetime.now()
+                time_string6 = now6.strftime("%H:%M:%S:%f")
+                self.serial.write(b'A6\n') #A6 Reading
+                value_A6 = float(self.serial.read_until(expected=b'\n').decode().strip())
+                self.dq.put([self.name+'6',value_A6])
+                print(f"-{self.name}6-{time_string6}T: {value_A6}")
+                barrier.wait(timeout=5)
 
-            now6 = datetime.now()
-            time_string6 = now6.strftime("%H:%M:%S:%f")
-            self.serial.write(b'A6\n') #A6 Reading
-            value_A6 = float(self.serial.read_until(expected=b'\n').decode().strip())
-            self.dq.put([self.name+'6',value_A6])
-            print(f"-{self.name}6-{time_string6}T: {value_A6}")
-            barrier.wait()
+                time.sleep(0.001)
 
-            if time.time() - temp_start > 12:
-                self.dq.put(["STOP", -1.0])
-                self.stop()
-
-            time.sleep(0.001) # delay: 1 ms
+            except: 
+                    self.dq.put(["STOP", -1.0])
+                    self.stop()
 
         print("Aquisition Finished!")
 
     def stop(self):
         self.run = False
+        self.serial.close()
+        print(f'Closed {self.name} Serial Connection')
